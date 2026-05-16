10c1740b51db558b67830a8bfdd7d90b4a77a083
piyushpatil1124 <piyushpatil1124@gmail.com>
2026-05-16 23:10:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
-from flask import Flask, render_template,
-render_template_string, request
+from flask import Flask, render_template,render_template_string, request
 import sys, os, random
 from datetime import datetime
 sys.path.insert(0, os.path.dirname(__file__))
